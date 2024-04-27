36b9c75bf8d4b15b5b104a05cd5decb724d8fae7
Harishrawal2 <harishrawal703@gmail.com>
2024-04-27 23:01:10 +0530

    signup, login, logout and middleware completed

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 import express from "express";
-import { config } from "dotenv";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-import { mongoDBConnection } from "./src/config/database.js";
 import bodyParser from "body-parser";
-
-// Resolve the directory of the current ES module file
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Configure dotenv to use .env file
-config({ path: `${__dirname}/.env` });
+import userRoutes from "./src/routes/userRoutes.js";
+import dotenv from "dotenv";
+import mongoDBConnection from "./src/config/database.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 // middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Mongodb connection
 mongoDBConnection();
+
+// environment configuration
+dotenv.config();
+
+// routes
+app.use("/api", userRoutes);
 
 // app configuration
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}`);
+  console.log(`Server listening on ${port}`);
 });
