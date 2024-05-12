e72c5c44c1a2e49260dfa492a0da9ada26a57e06
Harishrawal2 <harishrawal703@gmail.com>
2024-05-12 17:54:07 +0530

    signui completed

--- frontend/src/pages/auth/Signin.jsx
+++ frontend/src/pages/auth/Signin.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Signin() {
   return (
-    <div>
-      <h1>Signin</h1>
+    <div className="flex justify-center items-center text-white py-20">
+      <div className="w-[500px] bg-slate-900 px-10 py-10 rounded-sm">
+        <div className="mb-5 font-bold text-center text-2xl text-opacity-1">
+          <h1>Sign In</h1>
+        </div>
+        <form className="flex flex-col gap-5 w-[100%]">
+          <input
+            type="email"
+            className="py-2 px-2 rounded-full"
+            name="email"
+            placeholder="example@gmail.com"
+          />
+          <input
+            type="password"
+            className="py-2 px-2 rounded-full"
+            name="password"
+            placeholder="Password"
+          />
+          <button className="py-2 px-2 bg-slate-700 hover:bg-slate-500 transition-all 0.5s ease-in-out rounded-full uppercase">
+            Signin
+          </button>
+          <p className="text-center">
+            Create an account{" "}
+            <Link to="/signup" className="text-blue-500">
+              signup
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 }
